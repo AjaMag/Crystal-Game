4e56b00853a6329959ab276cd8d033c76d2fae99
Phoenix <aja.magdaleno@gmail.com>
2018-08-22 13:01:27 -0700

    "still not getting the currentScore to pass"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,15 @@ var images = ["images/blueCrystal.png", "images/multiCrystal.jpg",
 "images/orangeCrystal.png", "images/redCrystal.jpg"];
 var val = "";
 
-$(document).ready(function() {
-
     function randomTargetNum () {
         targetNum = Math.floor(Math.random() * 80) + 1;
-        $(".targetNum").text(`Target Number:${targetNum}`);
-        $(".yourScore").text(`Your Score: ${currentScore}`);
-        $(".wins").text(`Wins: ${wins}`);
-        $(".loses").text(`Loses: ${loses}`);
     }
-    randomTargetNum();
-    
+     
     function resetCrystals () {
 		for (var i = 0; i < images.length; i++) {
 			var crystal = $("<img>");
             crystal.addClass("crystal");
             crystal.attr("val", (Math.floor(Math.random() * 20) + 1));
-            let val = crystal.attr("val");
-            val.toString();
-            console.log(val);
-            crystal.attr("onClick", "clickCrystal()")
 			crystal.attr("src", images[i]);
             crystal.attr("height", "125px");
             crystal.attr("padding", "10px");
@@ -33,19 +22,32 @@ $(document).ready(function() {
             
         }
     }
-    resetCrystals();
-});
-    // function resetStats() {
-//  $(".crystal-images").empty();
-    // };
-    // function resetStats()
+    
+    function resetStats() {
+        randomTargetNum();
+        $(".targetNum").text(`Target Number:${targetNum}`);
+        currentScore = 0
+        $(".yourScore").text(`Your Score: ${currentScore}`);
+        $(".wins").text(`Wins: ${wins}`);
+        $(".loses").text(`Loses: ${loses}`);
+    }
+ 
+    resetStats();
 
-    function clickCrystal(event) {
-        currentScore += $('.crystal').attr(val);
-        $(".yourScore").text(currentScore);  
+    // function resetPlay(){
+    // $(".crystal-images").empty()     
+    //resetCrystals();
+  
+    // }
+
+    function clickCrystal() {
+        $(document).on("click", )
+        currentScore += $('.crystal').attr("val");
+        $(".yourScore").html(currentScore);  
     }
 
-// $(document).on("click", clickCrystal);
+
+// $(document).on("click", clickCrystal());
 
 
     
