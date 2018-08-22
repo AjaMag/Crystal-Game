6fb6a1cd7ae1392f6dfc211641ea90aa6980a188
Phoenix <aja.magdaleno@gmail.com>
2018-08-21 18:40:23 -0700

    "on click f(x) added"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,48 @@ let loses = 0;
 let currentScore = 0;
 let targetNum = "";
 var images = ["images/blueCrystal.png", "images/multiCrystal.jpg", 
-    "images/orangeCrystal.png", "images/redCrystal.jpg"];
+"images/orangeCrystal.png", "images/redCrystal.jpg"];
+let val = "";
+
+$(document).ready(function() {
 
     function randomTargetNum () {
         targetNum = Math.floor(Math.random() * 80) + 1;
-    };
+        $(".targetNum").text(`Target Number:${targetNum}`);
+        $(".yourScore").text(`Your Score: ${currentScore}`);
+        $(".wins").text(`Wins: ${wins}`);
+        $(".loses").text(`Loses: ${loses}`);
+    }
     randomTargetNum();
     
     function resetCrystals () {
 		for (var i = 0; i < images.length; i++) {
 			var crystal = $("<img>");
-			crystal.addClass("crystal");
+            crystal.addClass("crystal");
+            let val = crystal.attr("value", (Math.floor(Math.random() * 20) + 1));
+            console.log(val);
 			crystal.attr("src", images[i]);
-            crystal.attr("value", (Math.floor(Math.random() * 20) + 1));
             crystal.attr("height", "125px");
             crystal.attr("padding", "10px");
-			$(".crystal-images").append(crystal);
+            $(".crystal-images").append(crystal);    
         }
     }
     resetCrystals();
+});
+    // function resetStats() {
+//  $(".crystal-images").empty();
+    // };
+    // function resetStats()
+
+    function clickCrystal() {
+        console.log("click");
+        currentScore +=parseInt($(this).attr("value"));
+        $(".yourScore").text(currentScore);
+    }
+    // clickCrystal();
+   
+
+    // $(document).on("click", ".crystal", clickCrystal);
+
+
     
